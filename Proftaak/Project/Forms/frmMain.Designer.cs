a8fc7de60f7dd72fbd586cf71da27abf696f1fbb
Marvin <marvin.zwolsman@gmail.com>
2015-09-23 10:27:52 +0200

    Renamed form

--- Proftaak/Project/Forms/frmMain.Designer.cs
+++ Proftaak/Project/Forms/frmMain.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Project
+﻿namespace EvenementBeheerSysteem.Forms
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "frmMain";
         }
 
         #endregion
--- Proftaak/Project/Program.cs
+++ Proftaak/Project/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EvenementBeheerSysteem.Forms;
 
-namespace Project
+namespace EvenementBeheerSysteem
 {
     static class Program
     {
@@ -16,7 +17,7 @@ namespace Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
