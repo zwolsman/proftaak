32fbe99e92f61f1e4ddf803a438a056cda61f418
Rick Pijnenburg <rick.pie@hotmail.com>
2015-11-05 14:39:09 +0100

    Completed Materiaalbeheer. Added Evenement and Material to Materiaalbeheer.

---
+++ Proftaak/MateriaalBeheer/Classes/Evenement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateriaalBeheer.Classes
+{
+    public class Evenement
+    {
+        public int ID { get; set; } = -1;
+        public string Name { get; set; }
+        public List<Material> material { get; set; }
+
+        public Evenement()
+        {
+            material = new List<Material>();
+        }
+
+        public void AddMaterial(Material m)
+        {
+            material.Add(m);
+        }
+    }
+}
