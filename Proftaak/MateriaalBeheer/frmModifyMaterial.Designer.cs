0cea2bc1d5ec6122a890593535f81336cfcb2a5f
Marvin <marvin.zwolsman@gmail.com>
2015-11-06 14:01:37 +0100

    Fixed label placing of the label 'Events'

--- Proftaak/MateriaalBeheer/frmModifyMaterial.Designer.cs
+++ Proftaak/MateriaalBeheer/frmModifyMaterial.Designer.cs
@@ -59,7 +59,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Location = new System.Drawing.Point(19, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 2;
