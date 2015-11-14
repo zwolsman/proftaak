22b6d9d81a902c4cfe57137b5c684649342bd94b
Rick Pijnenburg <rick.pie@hotmail.com>
2015-11-14 21:17:36 +0100

    changed font

--- Proftaak/Toegangscontrole/frmToegangsControle.Designer.cs
+++ Proftaak/Toegangscontrole/frmToegangsControle.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 255);
-            this.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmToegangsControle";
             this.Text = "Toegangscontrole";
             this.ResumeLayout(false);
