df98f260cf655bc8c7a914cca5878f62d10c642f
Rick Pijnenburg <rick.pie@hotmail.com>
2015-11-16 15:23:18 +0100

    Added frmPresentieLijst

---
+++ Proftaak/Toegangscontrole/frmPresentieLijst.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Toegangscontrole.Classes;
+
+namespace Toegangscontrole
+{
+    public partial class frmPresentieLijst : Form
+    {
+        private Evenement evenement;
+
+        public frmPresentieLijst(Evenement e)
+        {
+            InitializeComponent();
+            evenement = e;
+        }
+    }
+}
