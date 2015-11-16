af8587233885890279286091d47804dc3a65ed96
Rick Pijnenburg <rick.pie@hotmail.com>
2015-11-16 15:23:00 +0100

    Added frmPortier

---
+++ Proftaak/Toegangscontrole/frmPortier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Toegangscontrole.Classes;
+
+namespace Toegangscontrole
+{
+    public partial class frmPortier : Form
+    {
+        private Evenement evenement;
+
+        public frmPortier(Evenement e)
+        {
+            InitializeComponent();
+            evenement = e;
+        }
+    }
+}
