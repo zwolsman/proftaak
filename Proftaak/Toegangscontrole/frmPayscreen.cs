3e205bbc27144cd06ea3673cb60883b19cf76585
Rick Pijnenburg <rick.pie@hotmail.com>
2015-11-17 15:16:31 +0100

    Added Payment and Rfid in Toegangscontrole.Classes Added frmPayscreen to Toegangscontrole

---
+++ Proftaak/Toegangscontrole/frmPayscreen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DatabaseLibrary;
+using Toegangscontrole.Classes;
+
+namespace Toegangscontrole
+{
+    public partial class frmPayscreen : Form
+    {
+        private float price;
+        private string rfid;
+
+        public frmPayscreen(int price, string rfid)
+        {
+            InitializeComponent();
+            DialogResult = DialogResult.Abort;
+            this.rfid = rfid;
+            this.price = price;
+            this.price = this.price / 100;
+            lblInfo.Text = "Nog te betalen: \u20AC" + this.price.ToString();
+        }
+
+        private void btPayNow_Click(object sender, EventArgs e)
+        {
+            int i = DatabaseManager.GetLeasePlaceID(rfid);
+            Payment p = new Payment()
+            {
+                LeasePlace = i,
+                Amount = (int)(price*100),
+                Description = "Betaald bij portier:" + price.ToString(),
+            };
+            DatabaseManager.InsertItem<Payment>(p);
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void lblInfo_SizeChanged(object sender, EventArgs e)
+        {
+            lblInfo.Left = (this.ClientSize.Width - lblInfo.Size.Width) / 2;
+        }
+    }
+}
--- Proftaak/Toegangscontrole/frmPortier.Designer.cs
+++ Proftaak/Toegangscontrole/frmPortier.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btBetaling = new System.Windows.Forms.Button();
+            this.btReservering = new System.Windows.Forms.Button();
+            this.btInschrijven = new System.Windows.Forms.Button();
+            this.btAanmelden = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btBetaling
+            // 
+            this.btBetaling.Location = new System.Drawing.Point(12, 12);
+            this.btBetaling.Name = "btBetaling";
+            this.btBetaling.Size = new System.Drawing.Size(190, 23);
+            this.btBetaling.TabIndex = 0;
+            this.btBetaling.Text = "Betalingen bekijken";
+            this.btBetaling.UseVisualStyleBackColor = true;
+            this.btBetaling.Click += new System.EventHandler(this.btBetaling_Click);
+            // 
+            // btReservering
+            // 
+            this.btReservering.Location = new System.Drawing.Point(12, 41);
+            this.btReservering.Name = "btReservering";
+            this.btReservering.Size = new System.Drawing.Size(190, 23);
+            this.btReservering.TabIndex = 1;
+            this.btReservering.Text = "Reservering bekijken";
+            this.btReservering.UseVisualStyleBackColor = true;
+            this.btReservering.Click += new System.EventHandler(this.btReservering_Click);
+            // 
+            // btInschrijven
+            // 
+            this.btInschrijven.Location = new System.Drawing.Point(12, 70);
+            this.btInschrijven.Name = "btInschrijven";
+            this.btInschrijven.Size = new System.Drawing.Size(190, 23);
+            this.btInschrijven.TabIndex = 2;
+            this.btInschrijven.Text = "Nieuwe inschrijving";
+            this.btInschrijven.UseVisualStyleBackColor = true;
+            this.btInschrijven.Click += new System.EventHandler(this.btInschrijven_Click);
+            // 
+            // btAanmelden
+            // 
+            this.btAanmelden.Location = new System.Drawing.Point(12, 99);
+            this.btAanmelden.Name = "btAanmelden";
+            this.btAanmelden.Size = new System.Drawing.Size(190, 23);
+            this.btAanmelden.TabIndex = 3;
+            this.btAanmelden.Text = "Aanmelden";
+            this.btAanmelden.UseVisualStyleBackColor = true;
+            this.btAanmelden.Click += new System.EventHandler(this.btAanmelden_Click);
             // 
             // frmPortier
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(214, 133);
+            this.Controls.Add(this.btAanmelden);
+            this.Controls.Add(this.btInschrijven);
+            this.Controls.Add(this.btReservering);
+            this.Controls.Add(this.btBetaling);
             this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmPortier";
             this.Text = "frmPortier";
@@ -43,5 +91,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btBetaling;
+        private System.Windows.Forms.Button btReservering;
+        private System.Windows.Forms.Button btInschrijven;
+        private System.Windows.Forms.Button btAanmelden;
     }
 }
